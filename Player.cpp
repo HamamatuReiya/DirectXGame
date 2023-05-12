67749a646d08271aa8f626b2540c5ac8e00b65cb
HAMA_ <HAMA_@MSI>
2023-05-12 15:25:20 +0900

    2-2途中

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,43 @@
 ﻿#include "Player.h"
 #include <cassert>
 
+Matrix4x4 Multiply(Matrix4x4 m1, Matrix4x4 m2) {
+	Matrix4x4 result;
+	result.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] +
+	                 m1.m[0][3] * m2.m[3][0];
+	result.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] +
+	                 m1.m[0][3] * m2.m[3][1];
+	result.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] +
+	                 m1.m[0][3] * m2.m[3][2];
+	result.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] +
+	                 m1.m[0][3] * m2.m[3][3];
+	result.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] +
+	                 m1.m[1][3] * m2.m[3][0];
+	result.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] +
+	                 m1.m[1][3] * m2.m[3][1];
+	result.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] +
+	                 m1.m[1][3] * m2.m[3][2];
+	result.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] +
+	                 m1.m[1][3] * m2.m[3][3];
+	result.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] +
+	                 m1.m[2][3] * m2.m[3][0];
+	result.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] +
+	                 m1.m[2][3] * m2.m[3][1];
+	result.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] +
+	                 m1.m[2][3] * m2.m[3][2];
+	result.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] +
+	                 m1.m[2][3] * m2.m[3][3];
+	result.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] +
+	                 m1.m[3][3] * m2.m[3][0];
+	result.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] +
+	                 m1.m[3][3] * m2.m[3][1];
+	result.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] +
+	                 m1.m[3][3] * m2.m[3][2];
+	result.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] +
+	                 m1.m[3][3] * m2.m[3][3];
+	return result;
+};
+
 void Player::Initialize(Model* model, uint32_t textureHandle) 
 { 
 	assert(model);
@@ -8,53 +45,25 @@ void Player::Initialize(Model* model, uint32_t textureHandle)
 	model_ = model;
 	textureHandle_ = textureHandle;
 
-	worldTransform_.Initialize();
 	//シングルトンインスタンスを取得する
 	input_ = Input::GetInstance();
 
 	//x,y,z方向のスケーリングを設定
 	worldTransform_.scale_ = {5.0f, 1.0f, 1.0f};
 
-	//ワールドトランスフォームの初期化
+	// x,y,z方向の回転を設定
+	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
+
+	// X,Y,Z方向のスケーリングを設定
+	worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
+
+	// ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
+
 }
 
 void Player::Updete()
 {
-	//スケーリング行列を宣言
-	Matrix4x4 matScale; // 4行4列
-
-	matScale.m[0][0] = worldTransform_.scale_.x;
-	matScale.m[1][1] = worldTransform_.scale_.y;
-	matScale.m[2][2] = worldTransform_.scale_.z;
-	matScale.m[3][3] = 1;
-
-	//x,y,z方向の回転を設定
-	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
-
-	//ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
-
-	//x軸回転行列を宣言
-	Matrix4x4 matRotX;
-	matRotX.m[0][0] = 1;
-	matRotX.m[1][1] = cosf(worldTransform_.rotation_.x);
-	matRotX.m[2][1] = -sinf(worldTransform_.rotation_.x);
-	matRotX.m[1][2] = sinf(worldTransform_.rotation_.x);
-	matRotX.m[2][2] = cosf(worldTransform_.rotation_.x);
-	matRotX.m[3][3] = 1;
-
-	// 行列の転送 行列の計算後に行う
-	worldTransform_.TransferMatrix();
-
-	worldTransform_.matWorld_;
-
-	worldTransform_.matWorld_ =
-	    worldTransform_.scale_ * worldTransform_.rotation_ * worldTransform_.matWorld_;
-
-	//行列を定数バッファに転送
-	worldTransform_.TransferMatrix();
-
 	//キャラクターの移動ベクトル
 	Vector3 move = {0, 0, 0};
 
@@ -80,7 +89,58 @@ void Player::Updete()
 	worldTransform_.translation_.y += move.y;
 	worldTransform_.translation_.z += move.z;
 
-	
+	// スケーリング行列を宣言
+	Matrix4x4 matScale; // 4行4列
+
+	matScale.m[0][0] = worldTransform_.scale_.x;
+	matScale.m[1][1] = worldTransform_.scale_.y;
+	matScale.m[2][2] = worldTransform_.scale_.z;
+	matScale.m[3][3] = 1;
+
+	// X軸回転行列を宣言
+	Matrix4x4 matRotX;
+	matRotX.m[0][0] = 1;
+	matRotX.m[1][1] = cosf(worldTransform_.rotation_.x);
+	matRotX.m[2][1] = -sinf(worldTransform_.rotation_.x);
+	matRotX.m[1][2] = sinf(worldTransform_.rotation_.x);
+	matRotX.m[2][2] = cosf(worldTransform_.rotation_.x);
+	matRotX.m[3][3] = 1;
+
+	// Y軸回転行列を宣言
+	Matrix4x4 matRotY;
+	matRotY.m[0][0] = cosf(worldTransform_.rotation_.y);
+	matRotY.m[1][1] = 1;
+	matRotY.m[0][2] = -sinf(worldTransform_.rotation_.y);
+	matRotY.m[2][0] = sinf(worldTransform_.rotation_.y);
+	matRotY.m[2][2] = cosf(worldTransform_.rotation_.y);
+	matRotY.m[3][3] = 1;
+
+	// Z軸回転行列を宣言
+	Matrix4x4 matRotZ;
+	matRotZ.m[0][0] = cosf(worldTransform_.rotation_.z);
+	matRotZ.m[1][0] = sinf(worldTransform_.rotation_.z);
+	matRotZ.m[0][1] = -sinf(worldTransform_.rotation_.z);
+	matRotZ.m[1][1] = cosf(worldTransform_.rotation_.z);
+	matRotZ.m[2][2] = 1;
+	matRotZ.m[3][3] = 1;
+
+	Matrix4x4 matRot = Multiply(matRotZ, Multiply(matRotX,matRotY));
+
+	// 平行移動行列の宣言
+	Matrix4x4 matTrans;
+
+	matTrans.m[0][0] = 1;
+	matTrans.m[1][1] = 1;
+	matTrans.m[2][2] = 1;
+	matTrans.m[3][3] = 1;
+	matTrans.m[3][0] = worldTransform_.translation_.x;
+	matTrans.m[3][1] = worldTransform_.translation_.y;
+	matTrans.m[3][2] = worldTransform_.translation_.z;
+
+	worldTransform_.matWorld_ = Multiply(matScale,Multiply(matRot ,matTrans));
+
+	// 行列の転送
+	worldTransform_.TransferMatrix();
 }
 
 void Player::Draw(ViewProjection& viewProjection) 
